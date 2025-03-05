54e523732c3c80e0fec7bf61ad8dc55952bbd644
Cotte10247 <131337695+Cotte10247@users.noreply.github.com>
2025-03-04 23:49:33 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
+    emptyOutDir: true
   },
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 }); 
